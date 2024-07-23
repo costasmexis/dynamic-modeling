ab17172199fd6db6ec94d42157e539921980bc5e
costasmexis <costasmexis@gmail.com>
2024-07-23 13:37:02 +0200

    Working on FBI

--- src/process.py
+++ src/process.py
@@ -59,7 +59,6 @@ class BatchProcess:
         plt.suptitle(title, y=1.01)
         plt.show()
 
-
 class FedBatchProcess:
     def __init__(
         self,
@@ -138,6 +137,105 @@ class FedBatchProcess:
         plt.legend()
         plt.xlabel("Time")
         plt.ylabel("Substrate")
+        plt.tight_layout()
+        plt.suptitle(title, y=1.01)
+        plt.show()
+
+class Induction:
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        feeds: pd.DataFrame,
+        mu_max: float,
+        Ks: float,
+        Yxs: float,
+    ):
+        self.df = df
+        self.T = self.df[self.df['Process'] == 'FBI']['Temperature'].unique()[0]
+        self.alpha = 0.001
+        self.feeds = feeds
+
+        self.t_span = (self.df["RTime"].values[0], self.df["RTime"].values[-1])
+        self.S0 = self.df["Glucose"].values[0]
+        self.X0 = self.df["Biomass"].values[0]
+        self.P0 = self.df["Protein"].values[0]
+        self.V0 = self.df["V"].values[0]
+
+        self.mu_max = mu_max
+        self.Ks = Ks
+        self.Yxs = Yxs
+
+        self.sol = None
+
+        self.Sin = 1.43 * 200  # Substrate concentration in feed (g/L)
+
+    def feeding_strategy(self, time):
+        for index, row in self.feeds.iterrows():
+            start_time = row["Time"]
+            end_time = row["Time"] + row["Duration"]
+            if start_time <= time < end_time:
+                return row["F"] / 1000
+        return 0
+
+    def system_ode(self, t, y):
+        X, S, P, V = y
+        
+        mu = self.mu_max * S / (self.Ks + S)  # Monod equation for specific growth rate
+        q_S = mu / self.Yxs  # Specific substrate uptake rate
+        r_p = self.alpha *  X
+        
+        F = self.feeding_strategy(t)
+
+        dVdt = F
+        dXdt = mu * X - X * (F / V)
+        dSdt = -q_S * X + (self.Sin - S) * (F / V)
+        dPdt = r_p - F / V * P
+
+        return [dXdt, dSdt, dPdt, dVdt]
+
+    def simulate(self, eval: bool = True) -> pd.DataFrame:
+        y0 = [self.X0, self.S0, self.P0, self.V0]
+
+        if eval:
+            self.sol = solve_ivp(
+                self.system_ode,
+                self.t_span,
+                y0,
+                t_eval=self.df["RTime"].values,
+            )
+        else:
+            t_eval = np.linspace(self.t_span[0], self.t_span[1], 1000)
+            self.sol = solve_ivp(self.system_ode, self.t_span, y0, t_eval=t_eval)
+
+    def plot_simulation(self, title: str) -> None:
+        plt.figure(figsize=(10, 10))
+        plt.subplot(3, 1, 1)
+        plt.plot(self.sol.t, self.sol.y[0], label="Biomass", color="green")
+        plt.scatter(
+            self.df["RTime"], self.df["Biomass"], color="green", label="Exp. Biomass"
+        )
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Biomass")
+        
+        plt.subplot(3, 1, 2)
+        plt.plot(self.sol.t, self.sol.y[1], label="Substrate", color="blue")
+        plt.scatter(
+            self.df["RTime"], self.df["Glucose"], color="blue", label="Exp. Glucose"
+        )
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Substrate")
+        
+        plt.subplot(3, 1, 3)
+        plt.plot(self.sol.t, self.sol.y[2], label="Protein", color="red")
+        plt.scatter(
+            self.df["RTime"], self.df["Protein"], color="red", label="Exp. Protein"
+        )
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Protein")
+
         plt.tight_layout()
         plt.suptitle(title, y=1.01)
         plt.show()
