4b251c5c4b96c25ced913bd37c825867acb7495a
costasmexis <costasmexis@gmail.com>
2024-08-19 18:47:16 +0200

    Finished BATCH BR01 - Todo: Start induction

--- src/batch_pinn.py
+++ src/batch_pinn.py
@@ -1,10 +1,8 @@
 from typing import Union
 
 import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
-from tqdm import tqdm
 import copy
 
 torch.manual_seed(42)
@@ -24,31 +22,21 @@ class PINN(nn.Module):
         self,
         input_dim: int,
         output_dim: int,
-        t_start: Union[np.float32, torch.Tensor],
-        t_end: Union[np.float32, torch.Tensor],
     ):
         super().__init__()
-        self.input = nn.Linear(input_dim, 25)
-        self.hidden = nn.Linear(25, 50)
-        self.hidden2 = nn.Linear(50, 50)
-        self.hidden3 = nn.Linear(50, 10)
-        self.output = nn.Linear(10, output_dim)
+        self.input = nn.Linear(input_dim, 64)
+        self.hidden = nn.Linear(64, 1024)
+        self.hidden2 = nn.Linear(1024, 1024)
+        self.hidden3 = nn.Linear(1024, 64)
+        self.output = nn.Linear(64, output_dim)
 
-        self.mu_max = nn.Parameter(torch.tensor([0.6]))
+        self.mu_max = nn.Parameter(torch.tensor([0.5]))
         self.K_s = nn.Parameter(torch.tensor([0.5]))
         self.Y_xs = nn.Parameter(torch.tensor([0.5]))
 
-        self.t_start = t_start
-        self.t_end = t_end
-        if isinstance(self.t_start, torch.Tensor):
-            self.t_start = self.t_start.item()
-        if isinstance(self.t_end, torch.Tensor):
-            self.t_end = self.t_end.item()
-
     def forward(self, x):
         x = torch.relu(self.input(x))
-        x = torch.relu(self.hidden(x))
-        x = torch.tanh(self.hidden2(x))
+        x = torch.tanh(self.hidden(x))
         x = torch.tanh(self.hidden2(x))
         x = torch.relu(self.hidden3(x))
         x = self.output(x)
@@ -95,7 +83,7 @@ def train(net, t, X_S, df, num_epochs=1000, verbose=True):
     best_model_weights = None
     patience = 1000
 
-    optimizer = torch.optim.Adam(net.parameters(), lr=5e-5)
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
 
     for epoch in range(num_epochs):
         optimizer.zero_grad()
@@ -104,13 +92,13 @@ def train(net, t, X_S, df, num_epochs=1000, verbose=True):
         loss_data = nn.MSELoss()(u_pred, X_S)
         loss_ic = nn.MSELoss()(u_pred[0], X_S[0]) 
         loss_pde = loss_ode(net, df["RTime"].min(), df["RTime"].max()) 
-
+        
         total_loss = loss_data + loss_ic + loss_pde
         total_loss.backward()
         optimizer.step()
 
         if verbose:
-            if epoch % 5000 == 0:
+            if epoch % 500 == 0:
                 print(f"Epoch {epoch} || Total Loss: {total_loss.item():.4f}, Loss Data: {loss_data.item():.4f}, Loss IC: {loss_ic.item():.4f}, Loss ODE: {loss_pde.item():.4f}")
                 print(f"mu_max: {net.mu_max.item():.4f}, K_s: {net.K_s.item():.4f}, Y_xs: {net.Y_xs.item():.4f}")   
         
