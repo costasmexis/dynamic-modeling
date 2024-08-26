571123e641dd0881669aac88138f0b0231c1222a
costasmexis <costasmexis@gmail.com>
2024-08-26 15:23:38 +0200

    working on induction seperately

---
+++ induction/machinelearning.py
@@ -0,0 +1,210 @@
+from typing import Union
+import numpy as np
+import pandas as pd
+import copy
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+from system_ode import Fs, Volume, K_S, Y_XS, S_IN, T_START, T_END
+
+# Reproducibility
+SEED = 42
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def numpy_to_tensor(array):
+    return (
+        torch.tensor(array, requires_grad=True, dtype=torch.float32)
+        .to(DEVICE)
+        .reshape(-1, 1)
+    )
+
+
+class PINN(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+    ):
+        super(PINN, self).__init__()
+        self.input = nn.Linear(input_dim, 50)
+        self.fc1 = nn.Linear(50, 300)
+        self.hidden = nn.Linear(300, 300)
+        self.fc2 = nn.Linear(300, 50)
+        self.output = nn.Linear(50, output_dim)
+
+        # Kinetic parameters
+        self.mu_max = nn.Parameter(torch.tensor([0.3]))
+        self.alpha = nn.Parameter(torch.tensor([0.3]))
+
+    def forward(self, x):
+        x = torch.tanh(self.input(x))
+        x = torch.tanh(self.fc1(x))
+        x = torch.relu(self.hidden(x))
+        x = torch.relu(self.hidden(x))
+        x = torch.tanh(self.fc2(x))
+        x = self.output(x)
+        return x
+
+
+def grad(outputs, inputs):
+    return torch.autograd.grad(
+        outputs, inputs, grad_outputs=torch.ones_like(outputs), create_graph=True
+    )
+
+
+def loss_fn(
+    net: torch.nn.Module,
+    t_start: Union[np.float32, torch.Tensor] = T_START,
+    t_end: Union[np.float32, torch.Tensor] = T_END,
+) -> torch.Tensor:
+    if isinstance(t_start, torch.Tensor):
+        t_start = t_start.item()
+    if isinstance(t_end, torch.Tensor):
+        t_end = t_end.item()
+
+    t = (
+        torch.linspace(
+            t_start,
+            t_end,
+            steps=50,
+        )
+        .view(-1, 1)
+        .requires_grad_(True)
+        .to(DEVICE)
+    )
+    F = torch.tensor([Fs(i) for i in t], dtype=torch.float32).view(-1, 1).to(DEVICE)
+    V = torch.tensor([Volume(i) for i in t], dtype=torch.float32).view(-1, 1).to(DEVICE)
+
+    u_pred = net.forward(t)
+    X_pred = u_pred[:, 0].view(-1, 1)
+    S_pred = u_pred[:, 1].view(-1, 1)
+    P_pred = u_pred[:, 2].view(-1, 1)
+
+    dXdt_pred = grad(X_pred, t)[0]
+    dSdt_pred = grad(S_pred, t)[0]
+    dPdt_pred = grad(P_pred, t)[0]
+
+    mu = net.mu_max * S_pred / (K_S + S_pred)
+
+    error_dXdt = nn.MSELoss()(dXdt_pred, mu * X_pred - X_pred * F / V)
+    error_dSdt = nn.MSELoss()(dSdt_pred, -mu * X_pred / Y_XS + F / V * (S_IN - S_pred))
+    error_dPdt = nn.MSELoss()(dPdt_pred, net.alpha * mu * X_pred - P_pred * F / V)
+
+    error_ode = error_dXdt + error_dSdt + error_dPdt
+
+    return error_ode
+
+
+def main(train_df: pd.DataFrame, full_df: pd.DataFrame, num_epochs: int = 10000):
+    t_train = numpy_to_tensor(train_df["RTime"].values)
+    X_train = numpy_to_tensor(train_df["Biomass"].values)
+    S_train = numpy_to_tensor(train_df["Glucose"].values)
+    P_train = numpy_to_tensor(train_df["Protein"].values)
+    u_train = torch.cat((X_train, S_train, P_train), dim=1).to(DEVICE)
+
+    net = PINN(1, 3).to(DEVICE)
+
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+
+    # Loss weights
+    w_data, w_ode, w_ic = 1, 1, 1
+
+    # Initialize early stopping variables
+    best_loss = float("inf")
+    best_model_weights = None
+    patience = 1000
+    threshold = 1e-2
+
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        u_pred = net.forward(t_train)
+
+        loss_data = nn.MSELoss()(u_pred, u_train) * w_data
+        loss_ode = loss_fn(net, T_START, T_END) * w_ode
+        loss_ic = nn.MSELoss()(u_pred[0, :], u_train[0, :]) * w_ic
+
+        loss = loss_data + loss_ode + loss_ic
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 100 == 0:
+            print(
+                f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}, Loss Data: {loss_data.item():.4f}, Loss ODE: {loss_ode.item():.4f}, Loss IC: {loss_ic.item():.4f}"
+            )
+            print(f"mu_max: {net.mu_max.item():.2f}, alpha: {net.alpha.item():.2f}")
+
+        if epoch >= 10000:
+            if loss < best_loss - threshold:
+                best_loss = loss
+                best_model_weights = copy.deepcopy(net.state_dict())
+                patience = 1000
+            else:
+                patience -= 1
+                if patience == 0:
+                    print(f"Early stopping at epoch {epoch}")
+                    net.load_state_dict(best_model_weights)
+                    break
+
+    t_test = numpy_to_tensor(full_df["RTime"].values)
+    u_pred = pd.DataFrame(net.forward(t_test).detach().cpu().numpy())
+    u_pred.columns = ["Biomass", "Glucose", "Protein"]
+    u_pred["RTime"] = t_test.detach().cpu().numpy()
+
+    return net, u_pred
+
+
+# Plot net predictions
+def plot_net_predictions(
+    full_df: pd.DataFrame, train_df: pd.DataFrame, u_pred: pd.DataFrame
+):
+    
+    _, ax = plt.subplots(1, 2, figsize=(12, 3))
+    ax[0].scatter(
+        full_df["RTime"],
+        full_df["Biomass"],
+        color="orange",
+        label="_Biomass",
+        alpha=0.3,
+    )
+    ax[0].scatter(
+        full_df["RTime"], full_df["Glucose"], color="green", label="_Glucose", alpha=0.3
+    )
+    ax[0].scatter(
+        train_df["RTime"],
+        train_df["Biomass"],
+        color="orange",
+        label="Biomass",
+        alpha=1.0,
+    )
+    ax[0].scatter(
+        train_df["RTime"],
+        train_df["Glucose"],
+        color="green",
+        label="Glucose",
+        alpha=1.0,
+    )
+    ax[1].scatter(
+        full_df["RTime"], full_df["Protein"], color="blue", label="_Protein", alpha=0.3
+    )
+    ax[1].scatter(
+        train_df["RTime"], train_df["Protein"], color="blue", label="Protein", alpha=1.0
+    )
+
+    ax[0].plot(u_pred["RTime"], u_pred["Biomass"], color="orange", label="Biomass_A")
+    ax[0].plot(u_pred["RTime"], u_pred["Glucose"], color="green", label="Glucose_A")
+    ax[1].plot(u_pred["RTime"], u_pred["Protein"], color="blue", label="Protein_A")
+
+    ax[0].legend()
+    ax[1].legend()
+
+    ax[0].set_xlabel("Time")
+    ax[0].set_ylabel("Concentration")
+    ax[1].set_xlabel("Time")
+    ax[1].set_ylabel("Concentration")
+
+    plt.show()
---
+++ induction/system_ode.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# Define parameters
+T_START, T_END = 0, 20
+NUM_SAMPLES = 25
+
+# Simulation time points
+t_sim = np.linspace(T_START, T_END, NUM_SAMPLES)
+
+# Kinetic parameters
+MU_MAX = 0.45  # 1/hour
+K_S = 0.20  # g/liter
+Y_XS = 0.41  # g/g
+S_IN = 10.0  # g/liter
+
+# Initial conditions
+X0, S0, P0, V0 = 0.05, 10.0, 0.0, 1.0
+F0 = 0.05  # Constant feed rate (liter/hour)
+IC = [X0, S0, P0, V0]
+
+# Function to calculate volume
+def Volume(t):
+    return V0 + F0*t
+
+def Fs(t):
+    return F0
+
+def mu(S, mumax, Ks):
+    return mumax * S / (Ks + S)
+
+def Rg(X, S, mumax, Ks):
+    return mu(S, mumax, Ks) * X
+
+def a(t):
+    return 0.6
+
+def simulate(
+    mumax: float = MU_MAX,
+    Ks: float = K_S,
+    Yxs: float = Y_XS,
+    Sin: float = S_IN,
+):
+
+    # differential equations
+    def SystemODE(x, t):
+        X, S, P, V = x
+        dX = -Fs(t) * X / V + Rg(X, S, mumax, Ks)
+        dP = -Fs(t) * P / V + a(t) * Rg(X, S, mumax, Ks)
+        dS = Fs(t) * (Sin - S) / V - Rg(X, S, mumax, Ks) / Yxs
+        dV = Fs(t)
+        return [dX, dS, dP, dV]
+
+    sol = odeint(SystemODE, IC, t_sim)
+
+    return sol.transpose()
+
+
+# Get dataset
+def GetDataset(
+    mumax: float = MU_MAX,
+    Ks: float = K_S,
+    Yxs: float = Y_XS,
+    Sin: float = S_IN,
+
+):
+    X, S, P, V = simulate(mumax, Ks, Yxs, Sin)
+    df = pd.DataFrame(
+        {"RTime": t_sim, "Biomass": X, "Glucose": S, "Protein": P, "V": V}
+    )
+    return df
+
+
+# Plot solution
+def PlotSolution(df: pd.DataFrame):
+    plt.figure(figsize=(12, 4))
+    plt.scatter(df['RTime'], df['Biomass'], label="Biomass", s=10, alpha=1)
+    plt.scatter(df['RTime'], df['Glucose'], label="Glucose", s=10, alpha=1)
+    plt.scatter(df['RTime'], df['Protein'], label="Protein", s=10, alpha=1)
+    plt.xlabel("Time (hours)")
+    plt.ylabel("Concentration (g/lt)")
+    plt.legend(loc="best")
+    plt.show()
+
+# Plot predictions vs actual
+def PlotPredictions(train_df: pd.DataFrame, df_pred: pd.DataFrame):
+    plt.figure(figsize=(12, 4))
+    plt.scatter(train_df['RTime'], train_df['Biomass'], label="_Biomass", s=10, alpha=0.3)
+    plt.scatter(train_df['RTime'], train_df['Glucose'], label="_Glucose", s=10, alpha=0.3)
+    plt.scatter(train_df['RTime'], train_df['Protein'], label="_Protein", s=10, alpha=0.3)
+    plt.plot(df_pred['RTime'], df_pred['Biomass'], label="Biomass", alpha=1)
+    plt.plot(df_pred['RTime'], df_pred['Glucose'], label="Glucose", alpha=1)
+    plt.plot(df_pred['RTime'], df_pred['Protein'], label="Protein", alpha=1)
+    plt.xlabel("Time (hours)")
+    plt.ylabel("Concentration (g/lt)")
+    plt.legend(loc="best")
+    plt.show()
