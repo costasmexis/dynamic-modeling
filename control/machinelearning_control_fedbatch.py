a07769b3711772dc5113c42bd1a7e225a3d736b4
costasmexis <costasmexis@gmail.com>
2024-09-04 12:40:27 +0200

    Working on fed batch control in silico data

--- control/machinelearning_control_fedbatch.py
+++ control/machinelearning_control_fedbatch.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("../")
 
 from typing import Union
@@ -8,19 +9,37 @@ import torch
 import torch.nn as nn
 import copy
 
-from src.utils import feeding_strategy, get_volume
+from system_ode_fedbatch import get_volume
 
-T_START = 0
-T_END = 10.5
 NUM_EPOCHS = 30000
 LEARNING_RATE = 1e-4
-NUM_POINTS = 10000
-NUM_COLLOCATION = 100000
+NUM_POINTS = 100
+NUM_COLLOCATION = 100
 PATIENCE = 1000
 THRESHOLD = 1e-3
 EARLY_STOPPING_EPOCH = 1
 
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def generate_dataset(full_df: pd.DataFrame) -> Union[torch.Tensor, torch.Tensor]:
+    df = pd.DataFrame(columns=["t", "Biomass", "Glucose"])
+    df["Biomass"] = np.random.uniform(2.5, 5, NUM_POINTS)
+    df["Glucose"] = full_df["Glucose"].iloc[0]
+    df["t"] = 0.0
+    df["F"] = np.random.uniform(0.015, 0.065, NUM_POINTS) 
+
+    print(f"Dataset shape: {df.shape}")
+
+    t_train = numpy_to_tensor(df["t"].values)
+    X_train = numpy_to_tensor(df["Biomass"].values)
+    S_train = numpy_to_tensor(df["Glucose"].values)
+    F_train = numpy_to_tensor(df["F"].values)
+
+    in_train = torch.cat([t_train, X_train, S_train], dim=1)
+    out_train = torch.cat([X_train, S_train], dim=1)
+    return in_train, out_train
+
 
 def numpy_to_tensor(array):
     return (
@@ -29,31 +48,35 @@ def numpy_to_tensor(array):
         .reshape(-1, 1)
     )
 
+
 def grad(outputs, inputs):
     return torch.autograd.grad(
         outputs, inputs, grad_outputs=torch.ones_like(outputs), create_graph=True
     )
 
+
 class PINN(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(PINN, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 128)
-        self.fc4 = nn.Linear(128, output_dim)
+        self.input = nn.Linear(input_dim, 64)
+        self.fc1 = nn.Linear(64, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 64)
+        self.output = nn.Linear(64, output_dim)
 
     def forward(self, x):
-        x = torch.tanh(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
-        x = torch.tanh(self.fc3(x))
-        x = self.fc4(x)
+        x = torch.relu(self.input(x))
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = self.output(x)
         return x
 
+
 def loss_fn(
-    net: nn.Module, 
+    net: nn.Module,
     t_start: Union[np.float32, torch.Tensor],
     t_end: Union[np.float32, torch.Tensor],
-    feeds: pd.DataFrame,
     Sin: float,
     V0: float,
     S0: float,
@@ -61,18 +84,15 @@ def loss_fn(
     K_s: float,
     Y_xs: float,
 ) -> torch.Tensor:
-
-    t_col = numpy_to_tensor(np.arange(t_start, t_end, 1)).to(DEVICE)
+    
+    t_col = numpy_to_tensor(np.linspace(t_start, t_end, NUM_COLLOCATION)).to(DEVICE)
     X_col = numpy_to_tensor(np.random.uniform(2.5, 5, NUM_COLLOCATION)).to(DEVICE)
     S_col = numpy_to_tensor([S0 for _ in range(len(t_col))]).to(DEVICE)
-    F_col = numpy_to_tensor(np.random.uniform(15, 35, NUM_COLLOCATION)).to(DEVICE)
-    V_col = (
-        torch.tensor(get_volume(feeds=feeds, V0=V0, t=t_col.cpu().detach().numpy().reshape(-1,)), requires_grad=True)
-        .view(-1, 1)
-        .to(DEVICE)
-    )
+    F_col = numpy_to_tensor(np.random.uniform(0.015, 0.065, NUM_COLLOCATION)).to(DEVICE)
+    F_col = numpy_to_tensor([0.01 for _ in range(len(t_col))]).to(DEVICE)
+    V_col = numpy_to_tensor([get_volume(t) for t in t_col]).to(DEVICE)
 
-    u_col = torch.cat((t_col, X_col, S_col, F_col), 1).to(DEVICE)
+    u_col = torch.cat((t_col, X_col, S_col), 1).to(DEVICE)
 
     preds = net.forward(u_col)
 
@@ -89,3 +109,60 @@ def loss_fn(
 
     error_ode = torch.mean(error_dXdt**2 + error_dSdt**2)
     return error_ode
+
+
+def main(
+    in_train: torch.Tensor,
+    out_train: torch.Tensor,
+    t_start: Union[np.float32, torch.Tensor],
+    t_end: Union[np.float32, torch.Tensor],
+    S_in: float,
+    V0: float,
+    S0: float,
+    mu_max: float,
+    Ks: float,
+    Yxs: float,
+):
+    
+    net = PINN(3, 2).to(DEVICE)
+    optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    # Loss weights
+    w_data, w_ode, w_ic = 1, 1, 1
+
+    # Initialize early stopping variables
+    best_loss = float("inf")
+    best_model_weights = None
+    patience = PATIENCE
+    threshold = THRESHOLD
+
+    for epoch in range(NUM_EPOCHS):
+        optimizer.zero_grad()
+        preds = net.forward(in_train)
+
+        loss_data = nn.MSELoss()(preds, out_train)
+
+        loss_ode = loss_fn(net, t_start, t_end, S_in, V0, S0, mu_max, Ks, Yxs)
+
+        loss = w_data * loss_data + w_ode * loss_ode
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 100 == 0:
+            print(
+                f"Epoch {epoch}, Loss_data: {loss_data.item():.4f}, Loss_ode: {loss_ode.item():.4f}"
+            )
+
+        if epoch >= EARLY_STOPPING_EPOCH:
+            if loss < best_loss - threshold:
+                best_loss = loss
+                best_model_weights = copy.deepcopy(net.state_dict())
+                patience = 1000
+            else:
+                patience -= 1
+                if patience == 0:
+                    print(f"Early stopping at epoch {epoch}")
+                    net.load_state_dict(best_model_weights)
+                    break
+
+    return net
---
+++ control/system_ode_fedbatch.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+from scipy.integrate import odeint
+
+T_START = 0
+T_END = 10.5
+NUM_SAMPLES = 25
+S0 = 0.01
+X0 = 4.16
+V0 = 1.55
+
+# parameter values
+mumax = 0.84     # 1/hour
+Ks = 0.2          # g/liter
+Yxs = 0.5         # g/g
+Sin = 1.43 * 200  # g/liter
+
+# inlet flowrate
+def Fs(t):
+    if t <= 4.73:
+        return 0.017
+    elif t <= 7.33:
+        return 0.031
+    elif t <= 9.17:
+        return 0.060
+    elif t <= 9.78:
+        return 0.031
+    else:
+        return 0.017
+
+def simulate(mumax, Ks, Yxs, Sin, T_START, T_END, NUM_SAMPLES):
+    
+    mumax = mumax
+    Ks = Ks
+    Yxs = Yxs
+    
+    # reaction rates
+    def mu(S):
+        return mumax*S/(Ks + S)
+
+    def Rg(X,S):
+        return mu(S)*X
+
+    # differential equations
+    def xdot(x,t):
+        X,S,V = x
+        dX = -Fs(t)*X/V + Rg(X,S)
+        dS = Fs(t)*(Sin-S)/V - Rg(X,S)/Yxs
+        dV = Fs(t)
+        return [dX,dS,dV]
+
+    IC = [X0, S0, V0]
+
+    t = np.linspace(T_START,T_END,NUM_SAMPLES)
+    sol = odeint(xdot,IC,t)
+
+    return sol
+
+def generate_data() -> pd.DataFrame:
+    X,S,V = simulate(mumax, Ks, Yxs, Sin, T_START, T_END, NUM_SAMPLES).transpose()
+    # Generate dataset 
+    t = np.linspace(T_START,T_END,NUM_SAMPLES)
+    full_df = pd.DataFrame({'RTime': t, 'Biomass': X, 'Glucose': S, 'V': V})
+    # Add smal noise to BIomass and Glucose
+    full_df['Biomass'] = full_df['Biomass'] + np.random.normal(0,0.2,NUM_SAMPLES)
+    full_df['Glucose'] = full_df['Glucose'] + np.random.uniform(0,0.1,NUM_SAMPLES)
+    return full_df
+
+def integrate_Fs(a, b):
+    integral = 0
+    if b <= 4.73:
+        integral = 0.017 * (b - a)
+    elif a <= 4.73 < b <= 7.33:
+        integral = 0.017 * (4.73 - a) + 0.031 * (b - 4.73)
+    elif a <= 4.73 < b <= 9.17:
+        integral = 0.017 * (4.73 - a) + 0.031 * (7.33 - 4.73) + 0.060 * (b - 7.33)
+    elif a <= 4.73 < b <= 9.78:
+        integral = 0.017 * (4.73 - a) + 0.031 * (7.33 - 4.73) + 0.060 * (9.17 - 7.33) + 0.031 * (b - 9.17)
+    elif a <= 4.73 < b:
+        integral = 0.017 * (4.73 - a) + 0.031 * (7.33 - 4.73) + 0.060 * (9.17 - 7.33) + 0.031 * (9.78 - 9.17) + 0.017 * (b - 9.78)
+    elif 4.73 < a <= 7.33 < b <= 9.17:
+        integral = 0.031 * (7.33 - a) + 0.060 * (b - 7.33)
+    elif 4.73 < a <= 7.33 < b <= 9.78:
+        integral = 0.031 * (7.33 - a) + 0.060 * (9.17 - 7.33) + 0.031 * (b - 9.17)
+    elif 4.73 < a <= 7.33 < b:
+        integral = 0.031 * (7.33 - a) + 0.060 * (9.17 - 7.33) + 0.031 * (9.78 - 9.17) + 0.017 * (b - 9.78)
+    elif 7.33 < a <= 9.17 < b <= 9.78:
+        integral = 0.060 * (9.17 - a) + 0.031 * (b - 9.17)
+    elif 7.33 < a <= 9.17 < b:
+        integral = 0.060 * (9.17 - a) + 0.031 * (9.78 - 9.17) + 0.017 * (b - 9.78)
+    elif 9.17 < a <= 9.78 < b:
+        integral = 0.031 * (9.78 - a) + 0.017 * (b - 9.78)
+    elif 9.17 < a <= 9.78 and b <= 9.78:
+        integral = 0.031 * (b - a)
+    elif 9.78 < a and 9.78 < b:
+        integral = 0.017 * (b - a)
+    return integral
+
+def get_volume(t):
+    if t <= 4.73:
+        return integrate_Fs(0, t) + V0
+    elif t <= 7.33:
+        return integrate_Fs(0, 4.73) + integrate_Fs(4.73, t) + V0
+    elif t <= 9.17:
+        return integrate_Fs(0, 4.73) + integrate_Fs(4.73, 7.33) + integrate_Fs(7.33, t) + V0
+    elif t <= 9.78:
+        return integrate_Fs(0, 4.73) + integrate_Fs(4.73, 7.33) + integrate_Fs(7.33, 9.17) + integrate_Fs(9.17, t) + V0
+    else:
+        return integrate_Fs(0, 4.73) + integrate_Fs(4.73, 7.33) + integrate_Fs(7.33, 9.17) + integrate_Fs(9.17, 9.78) + integrate_Fs(9.78, t) + V0
+    
